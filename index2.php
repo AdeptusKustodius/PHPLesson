ecea12b8d85881d1428fcf4534bb8c8c7dc7fd86
AdeptusKustodius <56783828+AdeptusKustodius@users.noreply.github.com>
2020-04-14 15:37:48 +0400

    asd
    первый файл с сравнениями типами данных, второй с выводом echo

---
+++ index2.php
@@ -0,0 +1,26 @@
+<?php
+        $num1 = 'godlikeasd';
+        $num2 = 'suprizzzzzeeeeee';
+        $num3 = date("Сегодня по Московскому времени: d.m.y H:i");
+        $a=1;
+        $b=2;
+
+        
+
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo($num1)?> </title>
+</head>
+<body>
+
+
+
+
+    <h1><?php echo($num2)?></h1>
+    <p><?php echo($num3)?></p>
+</body>
+</html>
